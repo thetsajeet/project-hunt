bca4c0eacbfb4a2a1250d0d3e8daec285cfa2b54
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-05-25 14:09:57 +0530

    drizzle orm with pg setup

--- project-hunt-server/README.md
+++ project-hunt-server/README.md
@@ -1,1 +1,12 @@
 # Project Hunt Server
+
+## Setup
+
+```bash
+cd project-hunt-server/
+cp .env.sample .env
+
+# Setup your env
+
+bun run dev
+```
---
+++ project-hunt-server/src/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  username: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password_hash: varchar({ length: 12 }).notNull(),
+});
