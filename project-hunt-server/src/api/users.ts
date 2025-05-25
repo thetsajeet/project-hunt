c214d3dbb4d5183bc1431033f38999968c1df93f
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-05-25 14:32:17 +0530

    get user details from db

--- project-hunt-server/src/api/users.ts
+++ project-hunt-server/src/api/users.ts
@@ -1,23 +1,51 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { db } from "../db";
+import { usersTable } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 const usersApi = new Hono();
 
 usersApi
-  .get("/", (c) => {
+  .get("/", async (c) => {
+    const users = await db
+      .select({
+        email: usersTable.email,
+        id: usersTable.id,
+        username: usersTable.username,
+      })
+      .from(usersTable);
+
     return c.json(
       {
         message: "ok",
-        data: [],
+        data: {
+          users,
+        },
       },
       200,
     );
   })
-  .get("/:id", (c) => {
-    const userId = c.req.param("id");
+  .get("/:id", async (c) => {
+    const userId = +c.req.param("id");
+    if (!userId)
+      throw new HTTPException(400, { message: `Invalid user id: ${userId}` });
+
+    const user = await db
+      .select({
+        email: usersTable.email,
+        id: usersTable.id,
+        username: usersTable.username,
+      })
+      .from(usersTable)
+      .where(eq(usersTable.id, userId));
 
     return c.json(
       {
         message: `ok`,
+        data: {
+          user,
+        },
       },
       200,
     );
--- project-hunt-server/src/db/index.ts
+++ project-hunt-server/src/db/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
 
-const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.DATABASE_URL, { schema });
